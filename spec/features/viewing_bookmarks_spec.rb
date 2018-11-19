48559bd081f36af94aec603c6df8d0c79f94943d
Max Stevenson <maxstevenson@msn.com>
2018-11-19 15:13:22 +0000

    - Created a bookmarks view that iterates through bookmarks instance variable and displays in an unordered list. - Created a features folder in spec for specific feature tests. - Developed a test for viewing bookmarks on the bookmarks route.

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -3,4 +3,11 @@ feature 'viewing bookmarks' do
 		visit ('/')
 		expect(page).to have_content("Bookmark Manager")
 	end
+
+	scenario 'visiting the bookmarks page' do
+		visit ('/bookmarks')
+		expect(page).to have_content("http://www.makersacademy.com")
+		expect(page).to have_content("http://www.destroyallsoftware.com")
+		expect(page).to have_content("http://www.google.com")
+	end
 end
