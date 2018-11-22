3ea204252389e70dce374dde2ddf4549969b0ce7
Max Stevenson <maxstevenson@msn.com>
2018-11-22 11:01:40 +0000

    - Developed a delete bookmark feature. - Added html delete button below each bookmark. - created a RESTful route that takes the id of the bookmark slated for deletion and passes it to a method in Bookmark that queries the database.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require './lib/bookmark'
 require 'pg'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions, :method_override
 
 	get '/' do
     'Bookmark Manager'
@@ -20,6 +21,11 @@ class BookmarkManager < Sinatra::Base
   post '/bookmark/new' do
   	Bookmark.create(url: params['url'], title: params['title'])
   	redirect ('/bookmarks')
+  end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params['id'])
+    redirect('/bookmarks')
   end
 
   run! if app_file == $0
